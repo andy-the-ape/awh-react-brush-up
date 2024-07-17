057292a00110f237fea052a4aca9e083ba069a67
Andreas Witten Honoré <honore.andreas@gmail.com>
2024-07-17 19:06:16 +0200

    Created more HTTP methods for Battle Cards

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -36,6 +36,7 @@ app.get('/health', (request: Request, response: Response) => {
     return response.status(200).send('Server is running');
 });
 
+// Create a new Battle Card and save it to the db
 app.post('/battle-card', async (request: Request, response: Response) => {
     try {
         if (
@@ -70,10 +71,75 @@ app.post('/battle-card', async (request: Request, response: Response) => {
     }
 });
 
+// Get all Battle Cards from the db
 app.get('/battle-cards', async (request: Request, response: Response) => {
     try {
         const battleCards: IBattleCard[] = await BattleCard.find();
-        return response.status(200).send(battleCards);
+        return response.status(200).send({
+            count: battleCards.length,
+            data: battleCards
+        });
+    } catch (error) {
+        console.error(error);
+        return response.status(500).send({ message: "An error occurred, please try again later." });
+    }
+});
+
+// Get a specific Battle Card by ID from the db
+app.get('/battle-cards/:id', async (request: Request, response: Response) => {
+    try {
+        const { id } = request.params;
+        const battleCard: IBattleCard | null = await BattleCard.findById(id);
+        if (!battleCard) {
+            return response.status(404).send({ message: "Battle Card not found." });
+        }
+        return response.status(200).send(battleCard);
+    } catch (error) {
+        console.error(error);
+        return response.status(500).send({ message: "An error occurred, please try again later." });
+    }
+});
+
+// Update a specific Battle Card by ID
+app.put('/battle-cards/:id', async (request: Request, response: Response) => {
+    try {
+        if (
+            !request.body.name ||
+            !request.body.type ||
+            !request.body.rarity ||
+            !request.body.health ||
+            !request.body.damage ||
+            !request.body.speed ||
+            !request.body.abilities ||
+            !request.body.imageUrl
+        ) {
+            return response.status(400).send({ message: "Missing required information." });
+        }
+
+        const { id } = request.params;
+
+        const result: IBattleCard | null = await BattleCard.findByIdAndUpdate(id, request.body);
+
+        if (!result) {
+            return response.status(404).send({ message: "Battle Card not found." });
+        }
+        return response.status(200).send({ message: "Battle Card updated successfully." });
+    } catch (error) {
+        console.error(error);
+        return response.status(500).send({ message: "An error occurred, please try again later." });
+    }
+});
+
+//Delete a specific Battle Card by ID
+app.delete('/battle-cards/:id', async (request: Request, response: Response) => {
+    try {
+        const { id } = request.params;
+        const result: IBattleCard | null = await BattleCard.findByIdAndDelete(id);
+
+        if (!result) {
+            return response.status(404).send({ message: "Battle Card not found." });
+        }
+        return response.status(200).send({ message: "Battle Card deleted successfully." });
     } catch (error) {
         console.error(error);
         return response.status(500).send({ message: "An error occurred, please try again later." });
